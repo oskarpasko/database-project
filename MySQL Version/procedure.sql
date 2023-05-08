953eae3d0e44c37c3dc8774f16d4e58a88e7f962
Oskar <oskarpasko@gmail.com>
2023-05-08 22:37:07 +0200

    Update procedure.sql

--- MySQL Version/procedure.sql
+++ MySQL Version/procedure.sql
@@ -29,10 +29,30 @@ SELECT @emp;
 
 #############################################################################
 ## procedura liczaca ilosc wykonanych przelewow przez danego klienta
-## procedura liczaca ilosc otrzymanych przelewow 
+DELIMITER $$
+$$
+CREATE PROCEDURE bankapp.client_overflow(IN cli CHAR(6), OUT emp INT)
+BEGIN
+	SELECT COUNT(*) INTO emp FROM detailed_overflow
+	WHERE client_nr  = cli;
+END$$
+DELIMITER ;
+
+## przykładowe wywołania procedury
+CALL client_overflow ('123456', @emp);
+SELECT @emp;
+
+CALL client_overflow ('654321', @emp);
+SELECT @emp;
+
+#############################################################################
 ## procedura liczaca ilosc pracownikow w kazdym miescie
+#############################################################################
 ## procedura liczaca klientow w kazdym miescie 
+#############################################################################
 ## procedura liczaca ilosc zadluzonych osob 
+#############################################################################
 ## procedura liczaca ilosc odzialow ogolnie
+#############################################################################
 ## procedura wyswietlja czy klient jest zadluzony czy nie 
 
