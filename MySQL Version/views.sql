c7f801f9884c724ec0e74471180b3e9411427a43
Oskar <>
2023-05-08 17:10:23 +0200

    Update views.sql

--- MySQL Version/views.sql
+++ MySQL Version/views.sql
@@ -1,13 +1,5 @@
 # Kody dodające widoki do bazy danych
 
-# Oskar
-
-# Eliza
-
-# Michał
-
-# Inne
-
 ##  card_client view
 CREATE VIEW card_client AS
 SELECT *
@@ -15,3 +7,52 @@ FROM card c
 LEFT JOIN client c2 
 USING(client_nr)
 ORDER BY client_nr 
+
+## pracownik view
+CREATE VIEW pracownik AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'pracownik'
+ORDER BY employee_lname
+
+## CEO view
+CREATE VIEW ceo AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'CEO'
+ORDER BY employee_lname
+
+## kierownik view
+CREATE VIEW kierownik AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'kierownik'
+ORDER BY employee_lname
+
+## menager view
+CREATE VIEW menager AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'menager'
+ORDER BY employee_lname
+
+## sprzedawca view
+CREATE VIEW sprzedawca AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'sprzedawca'
+ORDER BY employee_lname
+
+## ochroniarz view
+CREATE VIEW ochroniarz AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'ochroniarz'
+ORDER BY employee_lname
+
+## ksiegowy view
+CREATE VIEW ksiegowy AS
+SELECT *
+FROM employee e 
+WHERE employee_position = 'ksiegowy'
+ORDER BY employee_lname
