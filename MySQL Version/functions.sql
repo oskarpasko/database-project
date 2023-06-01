6be7ddf7bbb4663743bde912be7933dc01109423
Oskar <oskarpasko@gmail.com>
2023-06-01 09:08:10 +0200

    Update functions.sql

--- MySQL Version/functions.sql
+++ MySQL Version/functions.sql
@@ -7,18 +7,32 @@ dodatkowo są dodane przykłady wywołania ich
 ## Funkcja liczaca klientow w kazdym miescie 
 
 DELIMITER $$
-
 CREATE FUNCTION client_company_count(city VARCHAR(100)) 
 RETURNS VARCHAR(10)
 DETERMINISTIC
 BEGIN
 	DECLARE liczba VARCHAR(10);
-	
 	SELECT COUNT(*) INTO liczba FROM client_company_view WHERE company_city = city;
-
 	RETURN liczba;	
 END$$
 DELIMITER ;
 
 ## Przykłady wywołania
 SELECT client_company_count("Rzeszów")
+
+#############################################################################
+## Funckja zwracajaca sume wyplat dla pracownikow w poszczegolnej placowce
+
+DELIMITER $$
+CREATE FUNCTION company_salary(city VARCHAR(100)) 
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+	DECLARE suma VARCHAR(10);
+	SELECT SUM(position_salary) INTO suma FROM detailed_employee WHERE company_city = "Rzeszów";
+	RETURN suma;	
+END$$
+DELIMITER ;
+
+## Przykłady wywołania
+SELECT company_salary("Warszawa")
