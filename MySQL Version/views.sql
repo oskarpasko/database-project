7fe507fc00e6162fa315a95116549bed7834ffaa
Oskar <oskarpasko@gmail.com>
2023-06-01 09:21:47 +0200

    Update views.sql

--- MySQL Version/views.sql
+++ MySQL Version/views.sql
@@ -6,56 +6,56 @@ SELECT *
 FROM card c
 LEFT JOIN client c2 
 USING(client_nr)
-ORDER BY client_nr 
+ORDER BY client_nr ;
 
 ## pracownik view
 CREATE VIEW pracownik AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'pracownik'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## CEO view
 CREATE VIEW ceo AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'CEO'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## kierownik view
 CREATE VIEW kierownik AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'kierownik'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## menager view
 CREATE VIEW menager AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'menager'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## sprzedawca view
 CREATE VIEW sprzedawca AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'sprzedawca'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## ochroniarz view
 CREATE VIEW ochroniarz AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'ochroniarz'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## ksiegowy view
 CREATE VIEW ksiegowy AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'ksiegowy'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## klienci z kartami kredytowymi 
 CREATE VIEW credit_cards AS
@@ -64,14 +64,14 @@ FROM card c
 LEFT JOIN client
 USING(client_nr)
 WHERE card_type = 'Kredytowa'
-ORDER BY client_lname
+ORDER BY client_lname;
 
 ## zarzad view
 CREATE VIEW zarzad AS
 SELECT *
 FROM employee e 
 WHERE employee_position = 'CEO' OR employee_position = 'kierownik' OR employee_position = 'menager'
-ORDER BY employee_lname
+ORDER BY employee_lname;
 
 ## szczegółowe przelewy
 CREATE VIEW detailed_overflow AS
@@ -80,7 +80,7 @@ FROM overflow
 LEFT JOIN card  
 ON overflow.overflow_send_number = card.card_nr 
 LEFT JOIN client
-USING(client_nr)
+USING(client_nr);
 
 ## dane klientów i placówek do którychnależą
 CREATE VIEW client_company_view AS
@@ -88,4 +88,4 @@ SELECT *
 FROM client_company 
 LEFT JOIN client USING(client_nr)
 LEFT JOIN company USING(company_id)
-ORDER BY company_city
+ORDER BY company_city;
