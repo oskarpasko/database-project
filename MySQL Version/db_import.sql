7c97c5f00784af02b510b451c8df96c240dd46b6
Oskar <oskarpasko@gmail.com>
2023-06-05 12:30:08 +0200

    Update db_import.sql

--- MySQL Version/db_import.sql
+++ MySQL Version/db_import.sql
@@ -32,7 +32,7 @@ CREATE TABLE card
     card_cvc_number VARCHAR(3) NOT NULL CHECK(char_length(card_cvc_number)=3),
     card_type ENUM('Debetowa', 'Kredytowa'),
     card_balance DECIMAL(11,2) NOT NULL,
-    client_nr VARCHAR(6) NOT NULL CHECK(char_length(client_nr) = 6),
+    client_nr VARCHAR(6) NOT NULL,
     
     FOREIGN KEY (client_nr)
     REFERENCES client(client_nr)
@@ -55,8 +55,8 @@ INSERT INTO card values('9102910291029102', '2026-02-08', '520', 'Kredytowa', -1
 CREATE TABLE overflow
 (
 	overflow_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    overflow_send_number VARCHAR(16) NOT NULL CHECK(char_length(overflow_send_number)=16),
-    overflow_recipent_number VARCHAR(16) NOT NULL CHECK(char_length(overflow_recipent_number)=16),
+    overflow_send_number VARCHAR(16) NOT NULL,
+    overflow_recipent_number VARCHAR(16) NOT NULL,
     overflow_data DATE NOT NULL,
     overflow_amount DECIMAL(11,2) NOT NULL,
     
@@ -448,14 +448,6 @@ BEGIN
 END$$
 DELIMITER ;
 
-## wplace pracownikow
-CREATE view salaries AS
-SELECT employee_email, employee_fname, employee_lname, position_salary as placa_netto, brutto(position_salary, 0.17) as placa_brutto, position_name
-FROM employee
-LEFT JOIN positions 
-ON employee_position = position_name
-order by placa_brutto;
-
 #############################################################################
 ## Funckja obliczajace sume wynagrodzen w konkretnym budynku
 
@@ -472,74 +464,6 @@ BEGIN
 END$$
 DELIMITER ; 
 
-#############################################################################
-## Przykłady wywołania
-SELECT client_company_count("Rzeszów")
-SELECT city_salary("Warszawa")
-SELECT log_in("rzaoczny@bankapp.com", "61939237410")
-SELECT brutto(1000.0, 0.17)
-/*
-Kody dodające funkcje do bazy danych
-dodatkowo są dodane przykłady wywołania ich
-!!!!!! Uruchom plik bez przykładowych wywołań tylko same deklaracje funkcji !!!!!!
-*/
-
-#############################################################################
-## Funkcja liczaca klientow w kazdym miescie 
-
-DELIMITER $$
-CREATE FUNCTION client_company_count(city VARCHAR(100)) 
-RETURNS VARCHAR(10)
-DETERMINISTIC
-BEGIN
-	DECLARE liczba VARCHAR(10);
-	SELECT COUNT(*) INTO liczba FROM client_company_view WHERE company_city = city;
-	RETURN liczba;	
-END$$
-DELIMITER ;
-
-#############################################################################
-## Funckja zwracajaca sume wyplat dla pracownikow w poszczegolnej placowce
-
-DELIMITER $$
-CREATE FUNCTION city_salary(city VARCHAR(100)) 
-RETURNS VARCHAR(10)
-DETERMINISTIC
-BEGIN
-	DECLARE suma VARCHAR(10);
-	SELECT SUM(position_salary) INTO suma FROM detailed_employee WHERE company_city = "Rzeszów";
-	RETURN suma;	
-END$$
-DELIMITER ;
-
-#############################################################################
-## Funckja logujaca pracownika
-
-DELIMITER $$
-CREATE FUNCTION log_in(email VARCHAR(100), passwd VARCHAR(100)) 
-RETURNS VARCHAR(1)
-DETERMINISTIC
-BEGIN
-	DECLARE temp VARCHAR(1);
-	SELECT COUNT(*) INTO temp FROM employee WHERE employee_email = email AND employee_pesel = passwd;
-	RETURN temp;
-END$$
-DELIMITER ;
-
-#############################################################################
-## Funckja obliczajace wartosc brutto
-
-DELIMITER $$
-CREATE FUNCTION brutto(balance DECIMAL(11,2), tax FLOAT) 
-RETURNS DECIMAL(11,2)
-DETERMINISTIC
-BEGIN
-	DECLARE temp DECIMAL(7,2);
-	set temp = balance + (balance * cast(tax as decimal(7,2)));
-	RETURN temp;
-END$$
-DELIMITER ;
-
 ## wplace pracownikow
 CREATE view salaries AS
 SELECT employee_email, employee_fname, employee_lname, position_salary as placa_netto, brutto(position_salary, 0.17) as placa_brutto, position_name
@@ -547,22 +471,6 @@ FROM employee
 LEFT JOIN positions 
 ON employee_position = position_name
 order by placa_brutto;
-
-#############################################################################
-## Funckja obliczajace sume wynagrodzen w konkretnym budynku
-
-DELIMITER $$
-CREATE FUNCTION company_salary(city VARCHAR(100), street VARCHAR(100), nr VARCHAR(5), post_code VARCHAR(6)) 
-RETURNS VARCHAR(10)
-DETERMINISTIC
-BEGIN
-	DECLARE suma VARCHAR(10);
-	SELECT SUM(position_salary) INTO suma 
-	FROM detailed_employee 
-	WHERE company_city = city AND company_street = street AND company_nr = nr AND company_post_code = post_code;
-	RETURN suma;	
-END$$
-DELIMITER ; 
 
 /*
 Kody dodające procedury do bazy danych
